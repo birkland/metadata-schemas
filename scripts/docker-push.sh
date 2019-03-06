c21905c5ad9cbc348690ec078afb5444713c2c5a
Aaron Birkland <apb@jhu.edu>
2019-03-05 19:42:04 -0500

    Tweak docker push script
    Do not use build date for tags, and do not fail the script if
    DOCKER_USERNAME is undefined

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -4,15 +4,12 @@ DOCKER_REPO_NAME="oapass/schema-service"
 
 if [ ! -z ${DOCKER_USERNAME+x} ]; then
   echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-else
-  echo "No docker credentials provided"
-  exit 1
 fi
 	
 # If this is a tag, push a tag.  Otherwise, push to latest
 GIT_TAG=`git describe --tags 2>/dev/null`
 if [ -n "$GIT_TAG" ]; then
-    DOCKER_TAG=${GIT_TAG}-$(date -I)
+    DOCKER_TAG=${GIT_TAG}
     docker tag ${DOCKER_REPO_NAME}:latest ${DOCKER_REPO_NAME}:${DOCKER_TAG}
     docker push ${DOCKER_REPO_NAME}:${DOCKER_TAG}
 fi
